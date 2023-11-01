525fe113e6ab7133885bff9d54854c4157746d6b
Kevin Jimenez <kevinjdelgado1941@gmail.com>
2023-10-31 21:18:54 -0500

    Conteo

--- src/db.js
+++ src/db.js
@@ -4,7 +4,8 @@ async function connection() {
   try {
     const dbName = "BarberiaKevin";
     await mongoose.connect(
-      `mongodb+srv://kevin:kevin@atlascluster.d7pjldm.mongodb.net/${dbName}?retryWrites=true&w=majority`
+      //`mongodb+srv://kevin:kevin@atlascluster.d7pjldm.mongodb.net/${dbName}?retryWrites=true&w=majority`
+      `mongodb://mongo:gh2eBch3Cc--3HEBeFAG4e3DddHBGa5A@monorail.proxy.rlwy.net:11234`
 
     );
     console.log("Base de datos Mongo conectada");
